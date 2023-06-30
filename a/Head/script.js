636d748d7abada359dd9a41b02d5191f84937fe7
Marco-Aguiar <marcoaguiar14@gmail.com>
2023-06-30 20:16:04 -0300

    redrect

--- a/Head/script.js
+++ a/Head/script.js
@@ -4,11 +4,11 @@ const buttonVermelho = document.getElementById("botaoVermelho");
 
 
 button.onclick = function(){
-  window.location.href = "../models/projeto2/portifolio-main/index.html"
+  window.location.href = "./models/projeto2/portifolio-main/index.html"
 }
 
 buttonAzul.onclick = function(){
-  window.location.href = "../models/projeto3/Portifolio-main/index.html"
+  window.location.href = "./models/projeto3/Portifolio-main/index.html"
 }
 
 buttonVermelho.onclick = function(){
