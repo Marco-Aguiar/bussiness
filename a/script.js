7f0c8abbe0501d99336816f3050cb69521944914
Marco-Aguiar <marcoaguiar14@gmail.com>
2023-06-30 19:59:54 -0300

    first commit

---
+++ a/script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("botaoVerde");
+const buttonAzul = document.getElementById("botaoAzul");
+const buttonVermelho = document.getElementById("botaoVermelho");
+
+
+button.onclick = function(){
+  window.location.href = "./models/projeto2/portifolio-main/index.html"
+}
+
+buttonAzul.onclick = function(){
+  window.location.href = "./models/projeto3/Portifolio-main/index.html"
+}
+
+buttonVermelho.onclick = function(){
+  window.location = "https://aguiarprogrammer.vercel.app"
+}
